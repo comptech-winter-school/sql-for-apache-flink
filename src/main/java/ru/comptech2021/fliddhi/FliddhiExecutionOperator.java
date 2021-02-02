920c221d2c5f43b0510c9b95c65e3893509e8600
AlOb <obukhova.alisa@gmail.com>
2021-02-02 17:52:52 +0700

    StreamRecord<Row> to siddhi Event

--- src/main/java/ru/comptech2021/fliddhi/FliddhiExecutionOperator.java
+++ src/main/java/ru/comptech2021/fliddhi/FliddhiExecutionOperator.java
@@ -10,18 +10,15 @@ import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 import org.apache.flink.types.Row;
 
-public class FliddhiExecutionOperator
-        extends  AbstractStreamOperator<Row>
-        implements OneInputStreamOperator<Row, Row> {
+public class FliddhiExecutionOperator extends  AbstractStreamOperator<Row> implements OneInputStreamOperator<Row, Row> {
 
-    private SiddhiAppRuntime siddhiAppRuntime;
+    private SiddhiAppRuntime siddhiAppRuntime; // вынести в executionEnvironment
     private InputHandler inputHandler;
-
 
     public FliddhiExecutionOperator(SiddhiManager siddhiManager, String sql) {
         this.siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(sql);
         this.inputHandler = this.siddhiAppRuntime.getInputHandler("InputStream");
-        siddhiAppRuntime.start(); //todo where is shutdown?
+        siddhiAppRuntime.start(); //вынести в execution environment, try {} finally { siddhiApp.shutdown(); }
 
         siddhiAppRuntime.addCallback("OutputStream", new StreamCallback() {
             @Override
@@ -43,7 +40,18 @@ public class FliddhiExecutionOperator
         }
     }
 
-    private Event record2Event(StreamRecord<Row> record) { return null; } //todo
+    private Event record2Event(StreamRecord<Row> record) { //todo перенести в StreamSchema
+        Row row = record.getValue();
+        int rowSize = row.getArity();
+        Object[] fields = new Object[rowSize];
+        for (int i=0; i < rowSize; i++) {
+            fields[i] = row.getField(i); //todo надо еще вытащить названия и типы полей чтобы они соответствовали друг другу
+        }
+        return new Event(record.getTimestamp(), fields);
+    }
 
-    private StreamRecord<Row> event2Record(Event event) { return null; } //todo перенести в StreamSchema
+    private StreamRecord<Row> event2Record(Event event) {
+        // rows = siddhiApp.streamDefinitionMap.attributeList
+        return null;
+    }
 }
