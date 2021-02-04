5ebac0190005cf5e8da859ed46b3eddeff896f0e
AlOb <obukhova.alisa@gmail.com>
2021-02-04 15:21:29 +0700

    execution operator done

--- src/main/java/ru/comptech2021/fliddhi/FliddhiExecutionOperator.java
+++ src/main/java/ru/comptech2021/fliddhi/FliddhiExecutionOperator.java
@@ -6,73 +6,61 @@ import io.siddhi.core.event.Event;
 import io.siddhi.core.stream.input.InputHandler;
 import io.siddhi.core.stream.output.StreamCallback;
 import io.siddhi.query.api.SiddhiApp;
-import io.siddhi.query.api.definition.Attribute;
-import io.siddhi.query.api.definition.StreamDefinition;
 import org.apache.flink.streaming.api.operators.AbstractStreamOperator;
 import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.types.Row;
+import scala.Tuple2;
 
 import java.util.*;
 
-public class FliddhiExecutionOperator extends AbstractStreamOperator<FliddhiStream> implements OneInputStreamOperator<FliddhiStream, FliddhiStream> {
+public class FliddhiExecutionOperator extends AbstractStreamOperator<Tuple2<String, Row>> implements OneInputStreamOperator<Tuple2<String, Row>, Tuple2<String, Row>> {
 
-    private SiddhiManager siddhiManager;
-    private SiddhiAppRuntime siddhiAppRuntime;
+    private transient SiddhiManager siddhiManager; //возможно что-то здесь тосит удалить
+    private transient SiddhiApp siddhiApp;
+    private transient SiddhiAppRuntime siddhiAppRuntime;
+
+    private Collection<String> inputStreamsName;
+    private Collection<String> outputStreamsName;
     private transient HashMap<String, InputHandler> siddhiInputHandlers = new HashMap<>(); // HashMap<SiddhiInputStreamName, InputHandler>
 
     public FliddhiExecutionOperator(SiddhiApp siddhiApp, Collection<String> inputStreamsName, Collection<String> outputStreamsName) {
+        this.siddhiApp = siddhiApp;
+        this.inputStreamsName= inputStreamsName;
+        this.outputStreamsName = outputStreamsName;
+    }
+
+    @Override
+    public void open() throws Exception {
+        super.open();
+
         this.siddhiManager = new SiddhiManager();
-        this.siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(siddhiApp);
+        this.siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(this.siddhiApp);
 
         inputStreamsName.forEach(name -> siddhiInputHandlers.put(name, siddhiAppRuntime.getInputHandler(name)));
         outputStreamsName.forEach(name ->
                 siddhiAppRuntime.addCallback(name, new StreamCallback() {
                     @Override
                     public void receive(Event[] events) {
-                        collectElements(events);
+                        collectElements(name, events);
                     }
                 }));
+
+        siddhiAppRuntime.start();
     }
 
     @Override
-    public void processElement(StreamRecord<FliddhiStream> streamRecord) throws Exception {
-        String flinkStreamName = streamRecord.getValue().name();
-        siddhiInputHandlers.get(flinkStreamName).send(record2Event(streamRecord));
+    public void processElement(StreamRecord<Tuple2<String, Row>> streamRecord) throws Exception {
+        Row row = streamRecord.getValue()._2;
+        String flinkStreamName = streamRecord.getValue()._1;
+        siddhiInputHandlers.get(flinkStreamName).send(row2Event(row));
     }
 
-
-    private void collectElements(Event[] events) {
+    private void collectElements(String streamName, Event[] events) {
         for (Event event : events) {
-            StreamRecord<FliddhiStream> record = event2Record(event);
+            StreamRecord<Tuple2<String, Row>> record = new StreamRecord<>(new Tuple2<>(streamName, event2Row(event)));
             this.output.collect(record);
         }
-    }
-
-    private Event record2Event(StreamRecord<FliddhiStream> record) { //todo вынести в StreamSchema/Converter, отрефакторить метод
-        FliddhiStream stream = record.getValue();
-        String streamName = stream.name();
-
-        Map<String, StreamDefinition> siddhiStreamDefinition = siddhiAppRuntime.getStreamDefinitionMap();
-        //в случае если StreamDefinition пуст, объявляем его и наполняем аттрибутами
-        StreamDefinition streamDefinition = siddhiStreamDefinition.getOrDefault(streamName, new StreamDefinition());
-        streamDefinition.setId(streamName); //потенциально лишняя операция
-        List<Attribute> attributeList = streamDefinition.getAttributeList();
-        if (attributeList.isEmpty()){
-            attributeList.addAll(stream.dataStream().map(row -> new Attribute(???)));
-        }
-        attributeList.forEach(attr -> streamDefinition.attribute(attr.getName(), attr.getType()));
-
-        // convert fliddhi stream to event
-    }
-
-    private StreamRecord<FliddhiStream> event2Record(Event event) {
-        return null;
-    }
-
-    @Override
-    public void open() throws Exception {
-        super.open();
-        siddhiAppRuntime.start();
     }
 
     @Override
@@ -81,4 +69,26 @@ public class FliddhiExecutionOperator extends AbstractStreamOperator<FliddhiStre
         siddhiManager.shutdown();
         siddhiAppRuntime.shutdown();
     }
+
+    private Event row2Event(Row row) { //todo вынести в StreamSchema/Converter, отрефакторить метод
+        int rowSize = row.getArity();
+        Object[] fields = new Object[rowSize];
+        for (int i = 0; i < rowSize; i++) {
+            fields[i] = row.getField(i);
+        }
+        Event event = new Event();
+        event.setData(fields);
+        return event;
+    }
+
+    private Row event2Row(Event event) {
+        Object[] data = event.getData();
+        int dataLength = data.length;
+        Row row = new Row(dataLength); // todo set RowKind?
+        for (int i = 0; i < dataLength; i++) {
+            row.setField(i, data[i]);
+        }
+        return row;
+    }
+
 }
